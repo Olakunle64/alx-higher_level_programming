b5df20150a5dcd893ae5bfc9bca1b43b59d84665
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-04 20:18:40 +0000

    create a test folder

---
+++ 0x00-python-hello_world/TEST_FOLDER/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check if a singly list has a cycle in it
+ * @list: a pointer to listint_t
+ *
+ * Return: return 0 if there is no cycle, or 1 if there
+ * is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+
+	if (list == NULL)
+		return (0);
+	current = list;
+	while (current != NULL)
+	{
+		current = current->next;
+		if (current == list)
+			return (1);
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/TEST_FOLDER/README.md
@@ -0,0 +1,1 @@
+This directory is meant to test my c code
