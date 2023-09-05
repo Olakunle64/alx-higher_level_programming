2ff5b727fbc601ec723c0e45c9d6219464f40160
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-05 20:40:42 +0000

    hande ""

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,14 @@
 
 def uppercase(str):
     length = len(str)
+    if length == 0:
+        print("")
+        return
+    full_string = ""
     for ch in str:
         num = ord(ch)
         length -= 1
         if num in range(97, 123):
             num = num - 32
-            print("{}".format(chr(num)), end='\n' if length == 0 else '')
-        else:
-            print("{}".format(ch), end='\n' if length == 0 else '')
+        full_string += chr(num)
+    print(full_string)
