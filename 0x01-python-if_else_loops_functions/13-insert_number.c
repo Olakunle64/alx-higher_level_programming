c02cdfbb6e34e0bac238350869a6351014c2e783
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-05 22:13:36 +0000

    use typecasting

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -22,6 +22,7 @@ listint_t *insert_node(listint_t **head, int number)
 	newnode->n = number;
 	if (head == NULL || *head == NULL)
 	{
+		head = (listint_t **)newnode;
 		return (newnode);
 	}
 	current = *head;
