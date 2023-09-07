2652c427915aa4d32298d424cf2717abc0c28408
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-07 17:58:39 +0000

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import calculator_1 as calc
+
+    arg_count = len(sys.argv)
+    if arg_count < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    operator = ['+', '-', '*', '/']
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == operator[0]:
+        print("{} + {} = {}".format(a, b, a + b))
+    elif sys.argv[2] == operator[1]:
+        print("{} - {} = {}".format(a, b, a - b))
+    elif sys.argv[2] == operator[2]:
+        print("{} - {} = {}".format(a, b, a * b))
+    elif sys.argv[2] == operator[3]:
+        print("{} - {} = {}".format(a, b, a / b))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import py_compile
-    module_name = "./hidden_4.pyc"
-    load_module = py_compile.compile(module_name)
+    import importlib.util
+
+    mod_name = "hidden_4.pyc"
+    file_path = "/home/vagrant/alx-high_level_programming/0x02-python-import_modules/"
+    spec_file = importlib.util.spec_from_file_location(mod_name, file_path)
+    load_object = importlib.util.module_from_spec(spec_file)
+    spec_file.loader.exec_module(load_object)
     
-    for name in sorted(dir(load_module)):
+    for name in sorted(dir(load_object)):
         if '_' == name[0]:
             continue;
         print("{}".format(name))
