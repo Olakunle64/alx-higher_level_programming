3740162e11d3c2ed8fc4431445b13ae05ab27cd7
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-07 21:19:24 +0000

    reveal hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
 
     mod_name = "hidden_4.pyc"
     for name in dir(mod_name):
-        if '_' == name[0]:
+        if name[0] == '_' and name[1] == '_':
             continue
         print("{}".format(name))
