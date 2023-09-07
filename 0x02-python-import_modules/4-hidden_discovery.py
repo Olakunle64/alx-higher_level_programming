e4ca0b429661c44d55db7f9350ab638e558b0845
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-07 20:59:29 +0000

    remove the semicolon after the continue

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     mod_name = "hidden_4.pyc"
     for name in dir(mod_name):
         if '_' == name[0]:
-            continue;
+            continue
         print("{}".format(name))
