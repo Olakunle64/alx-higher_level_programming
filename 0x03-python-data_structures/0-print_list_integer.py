8742a0370f92fb5c82eabf00d72a0a1c9c5c5f41
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-09 16:45:38 +0000

    remove conflicting file and format the str.format as integer with :d

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -11,4 +11,4 @@ def print_list_integer(my_list=[]):
     """
     if my_list:
         for num in my_list:
-            print("{}".format(num))
+            print("{:d}".format(num))
--- 0x03-python-data_structures/test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from 0-print_list_integer import print_list_integer
-
-my_list = [1, 2, 3, 4]
-print_list_integer(my_list)
