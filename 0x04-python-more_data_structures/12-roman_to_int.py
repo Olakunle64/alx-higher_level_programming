80fcb02f465ad5f5769bf377c2f0b63ab6607193
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-13 21:35:05 +0000

    correct pycodestyle error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -19,21 +19,3 @@ def roman_to_int(roman_string):
         pre = post
         result += pre
     return result
-
-print(roman_to_int(""))
-print(roman_to_int(12))
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
