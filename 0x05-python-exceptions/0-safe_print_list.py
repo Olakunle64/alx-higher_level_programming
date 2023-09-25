0d2d3270288ac26e2645cf3f59e05f2ba93c9211
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-25 13:36:49 +0000

    make corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,6 +15,6 @@ def safe_print_list(my_list=[], x=0):
             elem_count += 1
         except IndexError:
             break
-    if my_list:
+    if my_list and x != 0:
         print()
     return elem_count
