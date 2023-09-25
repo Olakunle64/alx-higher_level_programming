e77a5a0e0292d4f9cbdc9168024bf77a45d5ac0d
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-25 12:29:38 +0000

    don't print a newline when the list is empty

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,8 +11,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
+            if i == x - 1:
+                print()
+                return i + 1
         except IndexError:
             print()
             return i
-    print()
-    return i + 1
