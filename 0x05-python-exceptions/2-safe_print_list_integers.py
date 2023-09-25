83a21e69604ecbdfa3e72276842273cf6a14673b
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-25 14:01:53 +0000

    print the first x elements of a list and only integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """print x elements of a list and only integers
+
+    Args:
+        my_list: a list
+        x: an integer
+
+    Return: the real number of integers printed
+    """
+    elem_count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            elem_count += 1
+        except ValueError:
+            pass
+    print()
+    return elem_count
