d87422d4972bc5cbd84c4bca2d1d120083fd4e28
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-25 14:56:36 +0000

    raise a name exception

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """rasie a name exception with message
+
+    Args:
+        message: a string
+
+    Return: void
+    """
+    raise NameError(message)
