5d0814594fd37dd7caee13777dde88df8a1404f5
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-25 14:08:03 +0000

    handle TypeError as well

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,7 +13,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             elem_count += 1
-        except ValueError:
+        except (ValueError, TypeError):
             pass
     print()
     return elem_count
