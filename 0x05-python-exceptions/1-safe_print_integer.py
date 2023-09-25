204f2ce329a9982bde5b518d8f11dd1ce4d8ced2
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-25 13:51:10 +0000

    print an integer in a safe way

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """print an integer with str.format
+
+    Args:
+        value: an integer
+
+    Return: True if value has been correctly
+    printed or false otherwise
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError:
+        return False
