9f5a32a6e3bc3f8c98cf629fbe5cd939ba94ab30
Olakunle64 <133286231+Olakunle64@users.noreply.github.com>
2023-09-25 22:27:25 +0100

    Update 102-magic_calculation.py
    Correct the bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -10,12 +10,11 @@ def magic_calculation(a, b):
     Return: void
     """
     result = 0
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            result = a ** b
-            result /= result
+            result = (a ** b) / i
         except Exception:
             result = a + b
             break
