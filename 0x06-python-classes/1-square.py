a244ac65adc8cc4c75e11bbbce977cf7d557a9c6
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-26 23:08:37 +0000

    create a class with a field attribute named size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""This file is that contains a class called Square
+
+    It has the special method and one field
+    """
+
+
+class Square:
+    """This class has the __init__ method
+
+    Attributes:
+        ___init__: first method
+
+    end
+
+    """
+    def __init__(self, size):
+        """Initializing self as a private attribute
+
+        Args:
+            size: an integer
+        """
+        self.__size = size
