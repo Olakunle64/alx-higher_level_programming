ba5433d4fcb736395e7195341317ad18f591dc39
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-26 23:44:36 +0000

    return the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""This file is that contains a class called Square
+
+    It has the special method and one field
+    """
+
+
+class Square:
+    """This class has the __init__ method
+
+    Attributes:
+        ___init__: first method
+        area: second method which is a public method
+
+    end
+
+    """
+    def __init__(self, size=0):
+        """Initializing self as a private attribute
+
+        Args:
+            size: an integer(ensure it is an integer and also
+            it must not be less than 0)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """return the area of a square"""
+        return (self.__size ** 2)
