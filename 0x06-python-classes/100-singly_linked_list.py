7e20845eb8b86e8597c62203e53ef0937bc3d540
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-27 17:01:38 +0000

    implement a singly linked list and use the __str__ method

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""This file contains two classes
+    
+    Aim: to implement the singly linked list
+    """
+class Node:
+    """The class is just like a node that has a
+    data and a pointer to the nextnode
+
+    Attributes:
+        data: field private attribute
+        next_node: field private attribute
+    """
+    def __init__(self, data, next_node=None):
+        """Initializing the field attribute"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """defining a getter for the data attribute"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """defining a setter for the data attribute"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """defining a getter for the next_node attribute"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """defining a setter for the next_node attribute"""
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+class SinglyLinkedList:
+    """This class is meant to insert a node in a list and also
+    define what would happen when the class is called with a print
+    function
+
+    Attributes:
+        __str__: method to define what will happen when the class is
+        called in a print function
+        sorted_insert: method to insert the newnode
+    """
+    def __init__(self):
+        """Initializing the pointer to the first node as a private field
+        with no getter and setter
+        """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """inserting a newnode of class Node"""
+        newnode = Node(value)
+        if self.__head is None or value < self.__head.data:
+            newnode.next_node = self.__head
+            self.__head = newnode
+        else:
+            current = self.__head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            newnode.next_node = current.next_node
+            current.next_node = newnode
+
+    def __str__(self):
+        """defining what will happend when the class is called with the print
+        function
+        """
+        result = []
+        current = self.__head
+        while current is not None:
+            result.append(str(current.data))
+            current = current.next_node
+        return "\n".join(result)
+
+
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+"""This file is that contains a class called Square
+
+    It has the special method and one field
+    """
+
+
+class Square:
+    """This class has the __init__ method
+
+    Attributes:
+        ___init__: first method
+        area: second method which is a public method
+        getter: a getter
+        setter: a setter
+        my_print: a method that print a square
+
+    end
+
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializing self as a private attribute
+
+        Args:
+            size: an integer(ensure it is an integer and also
+            it must not be less than 0)
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """a getter to get the value of the private attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """a setter to set the private attribute size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """a getter to get the value of the private attribute"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """a setter to set the private attribute position"""
+        if (
+            not isinstance(value, tuple) or
+            len(value) != 2 or not
+            all(isinstance(x, int) and x >= 0 for x in value)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """return the area of a square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """print a square with character <#> to stdout"""
+        if self.__size == 0:
+            print()
+            return
+        for yaxis in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for xaxis in range(self.__position[0]):
+                print(' ', end='')
+            for j in range(self.__size):
+                print('#', end='')
+            print()
+    def __str__(self):
+        """defining what would happen when this class is called
+        with a print function
+        """
+        if self.__size == 0:
+            return "\n"
+        result = []
+        result.append(str('\n' * self.__position[1]))
+        for i in range(self.__size):
+            result.append(str(' ' * self.__position[0] + '#' * self.__size))
+            result.append(str('\n' if i < self.__size - 1 else ""))
+        return ''.join(result)
+
