d11ed1b07532d0ec241e907acd2977236683529b
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-27 23:12:10 +0000

    import math module

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,6 +7,7 @@
 
     end
     """
+import math
 
 
 class MagicClass:
@@ -20,7 +21,6 @@ class MagicClass:
 
     def __init__(self, radius=0):
         """initializing radius"""
-        self.__radius = 0
         if type(radius) is not int:
             if type(radius) is not float:
                 raise TypeError("radius must be a number")
