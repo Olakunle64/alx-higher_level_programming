ae20d32fdaa88117909b5af4df605d6e1376e24b
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-27 01:35:00 +0000

    corrections

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,10 +51,14 @@ class Square:
     @position.setter
     def position(self, value):
         """a setter to set the private attribute position"""
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(x,
-            int) and x >= 0 for x in value):
+        if (
+            not isinstance(value, tuple) or
+            len(value) != 2 or not
+            all(isinstance(x, int) and x >= 0 for x in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """return the area of a square"""
@@ -64,10 +68,10 @@ class Square:
         """print a square with character <#> to stdout"""
         if self.__size == 0:
             print()
-        for l in range(self.__position[1]):
+        for yaxis in range(self.__position[1]):
             print()
         for i in range(self.__size):
-            for k in range(self.__position[0]):
+            for xaxis in range(self.__position[0]):
                 print(' ', end='')
             for j in range(self.__size):
                 print('#', end='')
