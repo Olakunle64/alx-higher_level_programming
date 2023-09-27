b8cc7e71116e0a9677b0e8b0038a67de744b297f
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-27 23:01:32 +0000

    initialize self.__radius to 0

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -18,12 +18,13 @@ class MagicClass:
         circumference: third method
     """
 
-    def __init__(self, radius=0):
+    def __init__(self, radius):
         """initializing radius"""
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
-        else:
-            self.__radius = radius
+        self.__radius = 0
+        if type(radius) is not int:
+            if type(radius) is not float:
+                raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
         """return the area of of the radius"""
