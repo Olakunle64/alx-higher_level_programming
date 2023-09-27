7d5d098b5846ef86026c822c2bea0552f7696f2d
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-27 00:11:31 +0000

    print a square to standard output

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""This file is that contains a class called Square
+
+    It has the special method and one field
+    """
+
+
+class Square:
+    """This class has the __init__ method
+
+    Attributes:
+        ___init__: first method
+        area: second method which is a public method
+        getter: a getter
+        setter: a setter
+        my_print: a method that print a square
+
+    end
+
+    """
+    def __init__(self, size=0):
+        """Initializing self as a private attribute
+
+        Args:
+            size: an integer(ensure it is an integer and also
+            it must not be less than 0)
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """a getter to get the value of the private attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """a setter to set the private attribute size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the area of a square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """print a square with character <#> to stdout"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print('#', end='')
+            print()
