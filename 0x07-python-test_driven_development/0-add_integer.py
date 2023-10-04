9887995abbb608b93a27e00a9854693dcfb19781
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-04 14:18:03 +0000

    correct pycodestyle error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     """return the addition of two integer
     Args: a(an integer), b(an integer), return the result
     """
-    
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
