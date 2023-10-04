74d32a196bd8cff94a1f84f967acc1783c266bf3
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-04 02:40:46 +0000

    corrections

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -24,8 +24,5 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
-    else:
-        for i in range(size):
-            print("#" * size)
+    for i in range(size):
+        print("#" * size)
