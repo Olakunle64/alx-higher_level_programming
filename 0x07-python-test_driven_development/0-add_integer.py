04459ebdbacef044b6e732be078705a9740325c6
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-04 14:16:12 +0000

    use try/except

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,11 +10,17 @@ def add_integer(a, b=98):
     """return the addition of two integer
     Args: a(an integer), b(an integer), return the result
     """
-
+    
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
+    try:
+        a = int(a)
+    except ValueError as errmsg:
+        raise ValueError(errmsg)
+    try:
+        b = int(b)
+    except ValueError as errmsg:
+        raise ValueError(errmsg)
     return a + b
