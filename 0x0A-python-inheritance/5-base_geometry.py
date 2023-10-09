d6e9571d516e8c88bb458928be877095160a192d
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-09 15:04:05 +0000

    define a class named BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a class named <BaseGeometry>
+
+    It is an empty class
+    """
+
+
+class BaseGeometry:
+    """This class has no attribute"""
+    pass
