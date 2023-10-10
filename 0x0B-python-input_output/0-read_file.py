2e7f9bf417c93662edfbb0a1c489838d181ca59c
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-10 14:29:44 +0000

    remove the newline at the end of what is printing to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,11 +4,13 @@
     Here is the guide to use the function:
     read_file(file_name)
     """
+
+
 def read_file(filename=""):
     """read a text file and print it to stdout"""
     if not filename or filename is None:
         return
     with open(filename, "r", encoding="utf-8") as file_object:
-        print(file_object.read())
-
-
+        text_read = file_object.read()
+        text_to_print = text_read[:len(text_read) - 1]
+        print(text_to_print)
