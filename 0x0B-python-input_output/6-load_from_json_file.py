c94d450f37934391e427e8fc20512c2f0d72feff
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-11 01:55:13 +0000

    load a json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module supplies a function <load_from_json_file>
+
+    Here is the guide to use the function:
+    load_from_json_file(filename)
+    """
+import json
+
+def load_from_json_file(filename):
+    """create an object from JSON file"""
+    with open(filename, "r", encoding="utf-8") as file_object:
+        return json.load(file_object)
