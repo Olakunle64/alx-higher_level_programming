7e622eec21bd0130494a73d95d26069e78cbc7c8
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-11 02:29:02 +0000

    corrections

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,6 +6,7 @@
     """
 import json
 
+
 def load_from_json_file(filename):
     """create an object from JSON file"""
     with open(filename, "r", encoding="utf-8") as file_object:
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,6 +3,7 @@
     to a python list and then save them to a file
     """
 
+import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -13,5 +14,6 @@ while i < len(sys.argv):
     new_list.append(sys.argv[i])
     i += 1
 save_to_json_file(new_list, "add_item.json")
+with open("add_item.json", "a", encoding="utf-8") as file_object:
+        file_object.write('\n')
 added_item = load_from_json_file("add_item.json")
-print(added_item)
