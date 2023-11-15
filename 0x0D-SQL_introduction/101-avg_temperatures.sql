8e226100fcee9bf343aea90a063dd44f6d51849a
Olakunle64 <olakunleisiaq50@gmail.com>
2023-11-15 01:29:21 +0000

    correct typo error

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Write a script that displays the average temperature (Fahrenheit)
 -- by city ordered by temperature (descending).
-SELECT city, AVG(value) AS average_temp FROM temperature GROUP BY city ORDER BY average_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
