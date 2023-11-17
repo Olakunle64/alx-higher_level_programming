bbdef616876c2f614fea744c3c911ea4c618ac3e
Olakunle64 <olakunleisiaq50@gmail.com>
2023-11-17 01:13:02 +0000

    sum up all rate for each show

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+
+SELECT ts.title, SUM(tsr.rate) AS rating
+FROM tv_shows ts JOIN tv_show_ratings tsr
+ON ts.id = tsr.show_id
+GROUP BY ts.title
+ORDER BY rating DESC;
