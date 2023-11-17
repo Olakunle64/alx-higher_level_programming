365ed312b98ff24c103f732d6cf4104080563e8a
Olakunle64 <olakunleisiaq50@gmail.com>
2023-11-17 01:19:10 +0000

    sum up all the rating for each genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,6 @@
+SELECT tg.name, SUM(tsr.rate) AS rating
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_show_ratings tsr ON tsg.show_id = tsr.show_id
+GROUP BY tg.name
+ORDER BY rating DESC;
