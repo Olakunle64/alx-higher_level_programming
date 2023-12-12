487e641ea18ecb650d3d9bd377144baeac0a6a86
Olakunle64 <olakunleisiaq50@gmail.com>
2023-12-12 22:35:04 +0000

    add more task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 
-/*Write a script that imports a dictionary of occurrences by user
+/* Write a script that imports a dictionary of occurrences by user
  * id and computes a dictionary of user ids by occurrence
  */
 
-dict = require('./101-data').dict
-big_dict = {}
+const dict = require('./101-data').dict;
+const bigDict = {};
 for (const key in dict) {
-	if (dict.hasOwnProperty(key)) {
-		const value = dict[key];
-		if
+  const value = dict[key];
+  if (!bigDict[dict[key]]) {
+    bigDict[value] = [];
+  }
+  bigDict[value].push(key);
+}
+console.log(bigDict);
