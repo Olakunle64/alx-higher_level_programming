47b9cedd34f7f95f255fab3b3026ab3211f10640
Olakunle64 <olakunleisiaq50@gmail.com>
2023-12-12 12:59:26 +0000

    create a class called Square that inherit from Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* create a class called Square that inherit from Rectangle */
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  size;
+  constructor (size) {
+    /* initialize instance attribute in both classes */
+    super(size, size);
+    this.size = size;
+  }
+};
