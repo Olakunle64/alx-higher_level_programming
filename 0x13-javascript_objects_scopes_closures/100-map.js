4ba188df7b8982725d836933253be4ab2e4b820e
Olakunle64 <olakunleisiaq50@gmail.com>
2023-12-12 17:18:28 +0000

    correct coding style error

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,12 +2,12 @@
 
 /* Write a script that imports an array and computes a new array */
 
-list = require('./100-data').list
+const list = require('./100-data').list;
 
 let index = -1;
-newList = list.map(function(element) {
-	index++;
-	return (element * index);
-})
+const newList = list.map(function (element) {
+  index++;
+  return (element * index);
+});
 console.log(list);
 console.log(newList);
