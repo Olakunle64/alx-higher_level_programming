baecab867671914d761c2c063d0a7c43b776523e
Olakunle64 <olakunleisiaq50@gmail.com>
2023-12-12 17:19:27 +0000

    correct task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -8,6 +8,5 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     /* initialize instance attribute in both classes */
     super(size, size);
-    this.size = size;
   }
 };
