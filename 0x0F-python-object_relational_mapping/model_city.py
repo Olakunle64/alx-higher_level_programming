998855a26d6a2626587fed3669054c1ee285c4c8
Olakunle64 <olakunleisiaq@gmail.com>
2023-12-15 15:25:13 +0000

    correct pycodestyle error

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""This module contains a class named State that inherit from
+"""This module contains a class named State and City that inherit from
     the Base class.
     """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
 Base = declarative_base()
 
 
@@ -19,14 +20,16 @@ class State(Base):
                 nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
+
 class City(Base):
     """A class which is assign a table in MYSQL database
 
-    It's has two class attributes(id and name)
+    It's has three class attributes(id, name and state_id)
+
+    Note: state_id is a foreign key referencing states.id
     """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, unique=True,
                 nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    referenced_table = relationship("State")
