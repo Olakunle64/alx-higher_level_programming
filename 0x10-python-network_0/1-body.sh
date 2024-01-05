26a5f782a5a830fc0db3c67b9c94c11b28dcb12f
Olakunle64 <olakunleisiaq@gmail.com>
2024-01-05 23:11:35 +0000

    no trailing newline

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Display the body of a 200 ok response
-#!/bin/bash
-[ "$(curl -s -o /dev/null -w '%{http_code}' "$1")" -eq 200 ] && curl -s "$1"
+[ "$(curl -s -o /dev/null -w '%{http_code}' "$1")" -eq 200 ] && body=$(curl -s "$1");echo -n "$body"
