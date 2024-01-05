c28009feb298318a9bbbd5e40824ecc22736eafe
Olakunle64 <olakunleisiaq@gmail.com>
2024-01-05 22:01:31 +0000

    make the script 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 # Write a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response and the size must be in byte
-
-if [ "$#" -gt 0 ]; then
-	url=$1
-	serverResponse=$(curl -sI "$url" 2>&1)
-	if [ "$?" -eq 0 ]; then
-		len=$(echo "$serverResponse" | grep "Content-Length" | grep -o '\b[0-9]\+\b')
-		echo "$len"
-	else
-		echo "Error"
-	fi
-else
-	echo "USAGE: <filename> <url>"
-fi
+	serverResponse=$(curl -sI "$1" 2>&1);len=$(echo "$serverResponse" | grep "Content-Length" | grep -o '\b[0-9]\+\b'); echo "$len"
