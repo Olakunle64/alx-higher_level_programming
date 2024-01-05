16a36adc1a8918905b7064c784901c54268e2a15
Olakunle64 <olakunleisiaq@gmail.com>
2024-01-05 23:54:00 +0000

    correction

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # list all available request methods available on the server
-methods=$(curl -si -X OPTIONS "$1" | awk '/Allow:/ {print $2}'); echo "$methods"
+methods=$(curl -si -X OPTIONS "$1" | awk '/Allow:/ {print $2, $3, $4}'); echo "$methods"
