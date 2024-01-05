f81162cc3151860aeac98a7f2c4a9ebe48ef7e3c
Olakunle64 <olakunleisiaq@gmail.com>
2024-01-05 22:02:46 +0000

    use the required shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Write a Bash script that takes in a URL, sends a request to that URL,
 	serverResponse=$(curl -sI "$1" 2>&1);len=$(echo "$serverResponse" | grep "Content-Length" | grep -o '\b[0-9]\+\b'); echo "$len"
