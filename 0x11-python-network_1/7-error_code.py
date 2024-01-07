d5d5cf6708434cddf9e41c0a5124c8d0590a16f3
Olakunle64 <133286231+Olakunle64@users.noreply.github.com>
2024-01-07 22:16:36 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """This module contain a Write a Python script that takes in a URL,
     sends a request to the URL and displays the body of the response
-    (decoded in utf-8).
+    using requests module.
     """
 
 
 if __name__ == "__main__":
-    import urllib.request
+    import requests
     import sys
 
     url = sys.argv[1]
-    response = request.get(url)
+    response = requests.get(url)
     if response.status_code >= 400:
         print(f'Error code: {response.status_code}')
     else:
